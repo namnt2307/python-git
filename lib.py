df6a06c36ec0a77b4235e5470bfe80b25692b548
nam.nguyen.tuan <nam.nguyen.tuan@vieon.vn>
2021-06-01 20:45:10 +0700

    edit main.py

--- lib.py
+++ lib.py
@@ -1,14 +1,19 @@
-import requests
-import json
-def create_var(pid: int,header,file):
-    file_name = open(f"{file}",'r',encoding='utf-8').read()
-    fields = json.loads(file_name)
-    a = requests.post(url=f'https://gitlab.vieon.vn/api/v4/projects/{pid}/variables',headers=header,data=fields)
-    print('HTTP Code: ',a.status_code, '\nResult: ', a.text)
+import requests, json
 
-def update_var(pid: int,header,file):
-    file_name = open(f"{file}",'r',encoding='utf-8').read()
-    fields = json.loads(file_name)
-    update_var = fields["key"]
-    a = requests.put(url=f'https://gitlab.vieon.vn/api/v4/projects/{pid}/variables/{update_var}',headers=header,data=fields)
-    print('HTTP Code: ',a.status_code, '\nResult: ', a.text)
+def create_project_var(pid: int, header: dict, file: str):
+
+    ### List các key trong project
+    check = requests.get(url = f'https://gitlab.vieon.vn/api/v4/projects/{pid}/variables', headers = header).text
+    check_list = [ key["key"] for key in json.loads(check) ]
+
+    with open(file, 'r', encoding='utf-8') as f:
+        file_data = f.read()
+        for key in json.loads(file_data):
+            if key["key"] in check_list: ### kiểm tra xem key có tồn tại trong project không
+                update_request = requests.put(url = f'https://gitlab.vieon.vn/api/v4/projects/{pid}/variables/{key["key"]}', headers = header, data = key)
+                print('Key: ', key["key"] , ' updated')
+
+            else:
+                create_request = requests.post(url = f'https://gitlab.vieon.vn/api/v4/projects/{pid}/variables', headers = header, data = key)
+                print('Key: ', key,' created')
+
--- main.py
+++ main.py
@@ -1,29 +1,31 @@
 import os
 import argparse
 from lib import *
-GITLAB_PRIVATE_TOKEN = os.getenv('GITLAB_PRIVATE_TOKEN',None)
-header = {}
+
+GITLAB_PRIVATE_TOKEN = os.getenv('GITLAB_PRIVATE_TOKEN', None)
+
+header = {
+    'PRIVATE-TOKEN': GITLAB_PRIVATE_TOKEN
+    }
+
+
+
 if __name__ == "__main__":
     
     if GITLAB_PRIVATE_TOKEN == None:
         print("""Please set GITLAB_PRIVATE_TOKEN before using pygit        
         """)
         exit(1)
-    else:
-        header = {
-            'PRIVATE-TOKEN': f'{GITLAB_PRIVATE_TOKEN}'
-        }
+
     parser = argparse.ArgumentParser(description="Git operator to add environment variables to Gitlab projects/groups")
-    parser.add_argument('action',help="Create project variables",choices=["create","update"])
-    parser.add_argument('--pid',help="Gitlab project ID",type=int,dest='pid')
-    parser.add_argument('--file',help="Variable json file",type=str,dest='file')
+    parser.add_argument('action', help="Create project variables", choices=["create"])
+    parser.add_argument('--pid', help="Gitlab project ID", type=int, dest='pid')
+    parser.add_argument('--file', help="Variable json file", type=str, dest='file')
 
     args = parser.parse_args()
 
     if args.action == "create":
-        create_var(args.pid,header,args.file)
-    elif args.action == "update":
-        update_var(args.pid,header,args.file)
+        create_project_var(args.pid, header, args.file)
 
     exit(0)
         
